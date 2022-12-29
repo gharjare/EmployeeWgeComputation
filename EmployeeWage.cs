85929c7775e9f3ecb8b139e6dd053f3d69548db0
gharjare <gharjarechetan@gmail.com>
2022-12-29 15:30:29 +0530

    [chetan gharjare]:Attendence/check attendence of employee

---
+++ EmployeeWage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    public class EmployeeWage
+    {
+        public static void Attendence()
+        {
+            int Is_Full_Time = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+            if(empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
