c2e088e5cb2d60192b9d01c36cbb9baead338a87
gharjare <gharjarechetan@gmail.com>
2022-12-29 15:27:27 +0530

    [chetan gharjare]: add objects in program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EmpWageComputation
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            // EmployeeCheck.CheckAttendence();
+            // EmployeeWage.CheckEmpWage();
+            // EmpPartTime.CheckPartTime();
+            //SwitchCase.CheckCases();
+            //  WagesForMonth.CalculateWageForMonth();
+            //  CalculateWagesInCondition.CheckCondition();
+        }
+    }
+}
