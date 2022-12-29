b5a801f4028594d86810b2ad7e52e448ce9da8df
gharjare <gharjarechetan@gmail.com>
2022-12-29 15:38:30 +0530

    [chetan gharjare]:Wages/ calculate employeeWage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,20 +8,27 @@ namespace EmpWageComputation
 {
     public class EmployeeWage
     {
-        public static void Attendence()
+        public static void CheckWages()
         {
             int Is_Full_Time = 1;
+            int Emp_Rate_per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(0,2);
             if(empCheck == Is_Full_Time)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empWage = 0;
             }
+            empWage = empHrs * Emp_Rate_per_Hour;
+            Console.WriteLine("Employeewage" + empWage);
         }
     }
 }
